55ed98b39b2d7fb652fb82b0785b0721ad0bd007
kai-xlr <xhauma@protonmail.com>
2025-07-02 10:54:34 -0400

    Created a simple http server. Deployed a simple welcome page

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kai-xlr/chirpy
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filePathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
