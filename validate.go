c9ed5600cf8ff221770371656ff4334857fe6879
kai-xlr <xhauma@protonmail.com>
2025-07-03 22:17:08 -0400

    Working on json encoder

---
+++ validate.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerValidate(w http.ResponseWriter, r *http.Request) {
+	type skeleton struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := skeleton{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+}
